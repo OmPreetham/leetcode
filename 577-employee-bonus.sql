d81399bdd93c351a9999b64072e5cd9c0004a65f
Om Preetham Bandi <ompreethambandi@gmail.com>
2025-04-06 18:49:34 -0500

    577

---
+++ 577-employee-bonus.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT 
+    Employee.name, Bonus.bonus
+FROM 
+    Employee
+LEFT OUTER JOIN 
+    Bonus
+ON 
+    Employee.empId = Bonus.empId
+WHERE 
+    bonus < 1000 OR bonus IS NULL
+;
+
