17b013b2b3a7a8c8b994ddc89803337dd264b0f3
Om Preetham Bandi <ompreethambandi@gmail.com>
2025-04-09 11:03:22 -0500

    242

---
+++ 242-valid-anagram.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    let count = new Map();
+
+    for (let char of s) {
+        count.set(char, (count.get(char) || 0) + 1);
+    }
+
+    for (let char of t) {
+        if (!count.has(char) || count.get(char) === 0) {
+            return false;
+        }
+        
+        count.set(char, count.get(char) - 1);
+    }
+
+    return true;
+};
